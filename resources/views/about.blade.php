43612710a560e24c64d3e91711c8b7970af5740e
Cahyo Susilo <cahyosusilo775@gmail.com>
2022-01-24 20:36:46 +0700

    Aplikasi Laravel Second Commit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,21 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.main')
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Cahyo Susilo | About</title>
-</head>
-
-<body>
-
-    <h1>Halaman About</h1>
-    <h3><?= $nama; ?></h3>
-    <p><?= $email; ?></p>
-    <img src="images/<?= $gambar ?>" alt="<?= $nama ?>" width="200px">
-
-</body>
-
-</html>
+@section('container')
+<h1>Halaman About</h1>
+<h3>{{ $nama }}</h3>
+<p>{{ $email }}</p>
+<img src="images/{{ $gambar }}" alt="{{ $nama }}" width="200px">
+@endsection
